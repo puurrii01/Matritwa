b2ff0818eea0bb21370cec31538c9c06fdedfb2b
puurrii01 <puurrii095@gmail.com>
2024-07-10 22:02:59 +0545

    started working on mobile version

--- app.js
+++ app.js
@@ -27,6 +27,19 @@ document.addEventListener('DOMContentLoaded', function() {
         return coordinates;
     }
 
+    var menubar = document.querySelector("#hamburger");
+    menubar.addEventListener("click", () => {
+        var menu = document.querySelector(".hide")
+        if (menubar.src.endsWith('menu.png')) {
+            menubar.src = './Images/close.png';
+            console.log('clicked');
+            menu.classList.add('extended-menu');
+        } else {
+            menubar.src = './Images/menu.png';
+            menu.classList.remove('extended-menu');
+        }
+    })
+
     // Initialize the Leaflet map
     function initMap() {
         const map = L.map('map').setView([27.7172, 85.3240], 12);
